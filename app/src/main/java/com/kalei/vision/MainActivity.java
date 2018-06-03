3810b3e6f9fdcef669d92c8ea3dbba07b48e55bc
Reid Isaki <reidisaki@yahoo.com>
2018-06-03 12:24:48 -0700

    fixed vision app for N+ devices

--- app/src/main/java/com/kalei/vision/MainActivity.java
+++ app/src/main/java/com/kalei/vision/MainActivity.java
@@ -42,15 +42,23 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.provider.Settings.Secure;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.Voice;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout.LayoutParams;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -60,6 +68,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     private static final String CLOUD_VISION_API_KEY = "AIzaSyCAUwAH8egdbmm5p8SsZEddWQEGR5_YmZE";
@@ -69,11 +79,15 @@ public class MainActivity extends AppCompatActivity {
     private static final int GALLERY_IMAGE_REQUEST = 1;
     public static final int CAMERA_PERMISSIONS_REQUEST = 2;
     public static final int CAMERA_IMAGE_REQUEST = 3;
+    private static final float PITCH_VALUE = 1.0f;
+    private static final float SPEECH_RATE = 1.0f;
     AsyncTask mTask;
     private TextView mImageDetails;
     private ImageView mMainImage;
     ProgressDialog mProgressLoading;
     private HashMap<String, String> mTranslateTable;
+    private TextToSpeech mTTS;
+    private LinearLayout mContainer;
 
     public void loadAds() {
 
@@ -83,13 +97,28 @@ public class MainActivity extends AppCompatActivity {
                     Secure.ANDROID_ID);
 //            AdRequest adRequest = new AdRequest.Builder().addTestDevice(android_id).build();
             AdRequest adRequest = new AdRequest.Builder().build();
-            mAdView.loadAd(adRequest);
+//            mAdView.loadAd(adRequest);
         }
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        mTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    mTTS.setLanguage(Locale.US);
+                }
+            }
+        });
+//        int result = mTTS.setLanguage(Locale.ENGLISH);
+//        if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+//            Log.e("TTS", "This Language is not supported");
+//            Intent installIntent = new Intent();
+//            installIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+//            startActivity(installIntent);
+//        }
         loadAds();
     }
 
@@ -97,33 +126,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         createHashTable();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new OnClickListener() {
             @Override
-            public void onClick(View view) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder
-                        .setMessage(R.string.dialog_select_prompt)
-                        .setPositiveButton(R.string.dialog_select_gallery, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                startGalleryChooser();
-                            }
-                        })
-                        .setNegativeButton(R.string.dialog_select_camera, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                startCamera();
-                            }
-                        });
-                builder.create().show();
+            public void onClick(final View v) {
+                startCamera();
             }
         });
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+//                builder
+//                        .setMessage(R.string.dialog_select_prompt)
+//                        .setPositiveButton(R.string.dialog_select_gallery, new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                startGalleryChooser();
+//                            }
+//                        })
+//                        .setNegativeButton(R.string.dialog_select_camera, new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                startCamera();
+//                            }
+//                        });
+//                builder.create().show();
+//            }
+//        });
 
+        mContainer = (LinearLayout) findViewById(R.id.container);
         mImageDetails = (TextView) findViewById(R.id.image_details);
         mMainImage = (ImageView) findViewById(R.id.main_image);
     }
@@ -164,6 +202,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         loadAds();
+        mContainer.removeAllViews();
         mMainImage.setVisibility(View.VISIBLE);
         if (requestCode == GALLERY_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
             uploadImage(data.getData());
@@ -221,9 +260,9 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Do the real work in an async task, because we need to use the network anyway
-        mTask = new AsyncTask<Object, Void, String>() {
+        mTask = new AsyncTask<Object, Void, List<String>>() {
             @Override
-            protected String doInBackground(Object... params) {
+            protected List<String> doInBackground(Object... params) {
                 try {
                     HttpTransport httpTransport = AndroidHttp.newCompatibleTransport();
                     JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
@@ -276,15 +315,41 @@ public class MainActivity extends AppCompatActivity {
                     Log.d(TAG, "failed to make API request because of other IOException " +
                             e.getMessage());
                 }
-                return "Cloud Vision API request failed. Check logs for details.";
+                return null;//"Cloud Vision API request failed. Check logs for details.";
             }
 
-            protected void onPostExecute(String result) {
+            protected void onPostExecute(List<String> result) {
                 mProgressLoading.dismiss();
-                mImageDetails.setText(result);
+//                if (label.getScore() * 100 > 75) {
+                String s = "";
+                for (String r : result) {
+                    s += r + "\n";
+                    Button row = new Button(MainActivity.this);
+                    row.setText(r);
+                    row.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT));
+                    row.setPadding(50, 50, 50, 50);
+                    row.setOnClickListener(new OnClickListener() {
+                        @Override
+                        public void onClick(final View v) {
+                            sayWord(((TextView) v).getText().toString());
+                        }
+                    });
+                    mContainer.addView(row);
+                }
+//                    stringList.add(label.getDescription());
+//                }
+                mImageDetails.setText(s);
             }
         };
         mTask.execute();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mTTS != null) {
+            mTTS.shutdown();
+        }
     }
 
     public Bitmap scaleBitmapDown(Bitmap bitmap, int maxDimension) {
@@ -307,9 +372,10 @@ public class MainActivity extends AppCompatActivity {
         return Bitmap.createScaledBitmap(bitmap, resizedWidth, resizedHeight, false);
     }
 
-    private String convertResponseToString(BatchAnnotateImagesResponse response) {
+    private List<String> convertResponseToString(BatchAnnotateImagesResponse response) {
         String message = "I found these things:\n\n";
 
+        List<String> stringList = new ArrayList<>();
         List<EntityAnnotation> labels = response.getResponses().get(0).getLabelAnnotations();
         if (labels != null) {
             for (EntityAnnotation label : labels) {
@@ -317,12 +383,15 @@ public class MainActivity extends AppCompatActivity {
                 message += String.format("%d%% : %s", Math.round(label.getScore() * 100),
                         lookUpHashValue(label.getDescription()) == null ? label.getDescription() : lookUpHashValue(label.getDescription()));
                 message += "\n";
+                if (label.getScore() * 100 > 75) {
+                    stringList.add(label.getDescription());
+                }
             }
         } else {
             message += "nothing";
         }
 
-        return message;
+        return stringList;
     }
 
     private void createHashTable() {
@@ -331,16 +400,47 @@ public class MainActivity extends AppCompatActivity {
         }
 
         //for smellia
-        mTranslateTable.put("model", "hag");
-        mTranslateTable.put("girl", "ho bag");
-        mTranslateTable.put("eyebrow", "manly brow");
-        mTranslateTable.put("eye brow", "manly brow");
-        mTranslateTable.put("beauty", "silly ho");
-        mTranslateTable.put("laptop", "stupid piece of metal");
-        mTranslateTable.put("woman", "troll");
-        mTranslateTable.put("child", "stumpy");
-        mTranslateTable.put("hair", "smelly");
-        mTranslateTable.put("toddler", "trilobyte");
+//        mTranslateTable.put("model", "hag");
+//        mTranslateTable.put("girl", "ho bag");
+//        mTranslateTable.put("eyebrow", "manly brow");
+//        mTranslateTable.put("eye brow", "manly brow");
+//        mTranslateTable.put("beauty", "silly ho");
+//        mTranslateTable.put("laptop", "stupid piece of metal");
+//        mTranslateTable.put("woman", "troll");
+//        mTranslateTable.put("child", "stumpy");
+//        mTranslateTable.put("hair", "smelly");
+//        mTranslateTable.put("toddler", "trilobyte");
+    }
+
+    protected void setupVoice() {
+
+        mTTS.setSpeechRate(SPEECH_RATE);
+        Set<Voice> voices = mTTS.getVoices();
+
+        if (voices != null) {
+            for (Voice v : voices) {
+                //could use a british voice here
+                //todo: make users download a language pack when they open the app
+                if (v.getLocale().equals(Locale.getDefault()) && v.getQuality() == Voice.QUALITY_HIGH) {
+                    Log.i("reid", v.toString());
+//                mTTS.setVoice(v);
+                }
+                if (v.getQuality() == Voice.QUALITY_HIGH && v.getLatency() == Voice.LATENCY_LOW && v.getLocale().equals(Locale.getDefault())) {
+//                    mTTS.setVoice(v);
+                }
+            }
+        }
+        mTTS.setPitch(PITCH_VALUE);
+//        mTTS.speak("ka", TextToSpeech.QUEUE_FLUSH, null, "");
+
+    }
+
+    protected void sayWord(String word) {
+        setupVoice();
+
+        //todo: handle other languages
+        mTTS.setLanguage(Locale.ENGLISH);
+        mTTS.speak(word, TextToSpeech.QUEUE_FLUSH, null, "");
     }
 
     private String lookUpHashValue(String key) {
